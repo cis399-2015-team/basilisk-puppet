698d02cbf3ca1aab1aad2bb267a5749e2557bb70
hkhamm <hkhamm@gmail.com>
2015-07-13 14:39:09 -0700

    adding apache2 module

---
+++ modules/httpd/manifests/init.pp
@@ -0,0 +1,30 @@
+class apache2 {
+    package {
+        'apache2': ensure => installed;
+    }
+
+    file { '/etc/apache2/apache2.conf':
+        source  => 'puppet:///modules/apache2/apache2.conf',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['apache2'],
+    }
+
+    file { '/var/www/html/index.html':
+        source  => 'puppet:///modules/apache2/index.html',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['apache2'],
+    }
+
+    service { 'httpd':
+        enable    => true,
+        ensure    => running,
+        require   => [ Package['apache2'],
+                       File['/etc/apache2/apache2.conf'],
+                       File['/etc/apache2/index.html']],
+        subscribe => File['/etc/apache2/apache2.conf'],
+    }
+}
