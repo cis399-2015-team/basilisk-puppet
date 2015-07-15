176e36c7f2212e3c9e5b323d9f9ca86ee98cc24f
hkhamm <hkhamm@gmail.com>
2015-07-15 14:35:38 -0700

    adding hostname and hosts files, removing master.cf

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -7,6 +7,22 @@ class postfix {
         'mailutils': ensure => installed;
     }
 
+    file { '/etc/hosts':
+        source  => 'puppet:///modules/postfix/hosts',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['postfix'],
+    }
+
+    file { '/etc/hostname':
+        source  => 'puppet:///modules/postfix/hostname',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['postfix'],
+    }
+
     file { '/etc/postfix/main.cf':
         source  => 'puppet:///modules/postfix/main.cf',
         mode    => 644,
@@ -19,7 +35,9 @@ class postfix {
         enable    => true,
         ensure    => running,
         require   => [ Package['postfix'],
-                       File['/etc/postfix/main.cf'] ],
+                       File['/etc/postfix/main.cf'],
+                       File['/etc/hosts'],
+                       File['/etc/hostname'] ],
         subscribe => File['/etc/postfix/main.cf'],
     }
 }
