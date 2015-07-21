d3bb2970b4b36fab3f5a83ee7c0c8a93bf391a85
hkhamm <hkhamm@gmail.com>
2015-07-21 14:57:06 -0700

    add image to site

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -19,12 +19,21 @@ class apache2 {
         require => Package['apache2'],
     }
 
+    file { '/var/www/html/dino-riders.jpg':
+        source  => 'puppet:///modules/apache2/dino-riders.jpg',
+        mode    => 644,
+        owner   => root,
+        group   => root,
+        require => Package['apache2'],
+    }
+
     service { 'apache2':
         enable    => true,
         ensure    => running,
         require   => [ Package['apache2'],
                        File['/etc/apache2/apache2.conf'],
                        File['/var/www/html/index.html']],
+                       File['/var/www/html/dino-riders.jpg']],
         subscribe => File['/etc/apache2/apache2.conf'],
     }
 }
