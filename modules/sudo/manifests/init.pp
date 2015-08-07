1fa1e4c94ba1b5e385afe4d502a7179ed210f7b0
hkhamm <hkhamm@gmail.com>
2015-08-07 15:10:17 -0700

    really do what I just thought I was doing

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,16 +1,16 @@
-class { 'sudo': }
+class sudo {
+    conf { 'admin':
+        ensure  => present,
+        content => '%admin ALL=(ALL) ALL',
+    }
 
-sudo::conf { 'admin':
-    ensure  => present,
-    content => '%admin ALL=(ALL) ALL',
-}
+    conf { 'samba':
+        ensure  => present,
+        content => '%samba ALL=(ALL) /bin/mount,/bin/umount,/sbin/mount.cifs,/sbin/umount.cifs',
+    }
 
-sudo::conf { 'samba':
-    ensure  => present,
-    content => '%samba ALL=(ALL) /bin/mount,/bin/umount,/sbin/mount.cifs,/sbin/umount.cifs',
-}
-
-sudo::conf { 'sudo':
-    ensure  => present,
-    content => '%sudo ALL=(ALL:ALL) ALL',
+    conf { 'sudo':
+        ensure  => present,
+        content => '%sudo ALL=(ALL:ALL) ALL',
+    }
 }
