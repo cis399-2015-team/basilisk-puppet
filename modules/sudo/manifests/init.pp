462c1afcad8ea20f641311605f9050d6c2cee671
hkhamm <hkhamm@gmail.com>
2015-08-07 15:11:33 -0700

    one more time

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,16 +1,16 @@
-class sudo {
-    conf { 'admin':
-        ensure  => present,
-        content => '%admin ALL=(ALL) ALL',
-    }
+class { 'sudo': }
 
-    conf { 'samba':
-        ensure  => present,
-        content => '%samba ALL=(ALL) /bin/mount,/bin/umount,/sbin/mount.cifs,/sbin/umount.cifs',
-    }
+sudo::conf { 'admin':
+    ensure  => present,
+    content => '%admin ALL=(ALL) ALL',
+}
 
-    conf { 'sudo':
-        ensure  => present,
-        content => '%sudo ALL=(ALL:ALL) ALL',
-    }
+sudo::conf { 'samba':
+    ensure  => present,
+    content => '%samba ALL=(ALL) /bin/mount,/bin/umount,/sbin/mount.cifs,/sbin/umount.cifs',
+}
+
+sudo::conf { 'sudo':
+    ensure  => present,
+    content => '%sudo ALL=(ALL:ALL) ALL',
 }
