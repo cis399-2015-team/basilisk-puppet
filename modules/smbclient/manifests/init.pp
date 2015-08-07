377051cf89c91fdf2f80870277d40a71ba248dbf
hkhamm <hkhamm@gmail.com>
2015-08-07 16:08:42 -0700

    added sudoers file to smbclient, changed reference

--- modules/smbclient/manifests/init.pp
+++ modules/smbclient/manifests/init.pp
@@ -11,11 +11,11 @@ class smbclient {
         groups     => ['samba'],
         membership => minimum,
     }
-    
+
     file { '/etc/sudoers':
         mode   => 440,
         owner  => root,
         group  => root,
-        source => 'puppet:///modules/sudo/sudoers'
+        source => 'puppet:///modules/smbclient/sudoers'
     }
 }
