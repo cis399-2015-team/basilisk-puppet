bbc1daf1140ab34cc2734dcacf7e6a14a1bd51d4
hkhamm <hkhamm@gmail.com>
2015-08-07 15:51:19 -0700

    doing it the "wrong" way: manually coping in a new sudoers file

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,4 +1,8 @@
 class sudo {
-    purge               => false,
-    config_file_replace => false,
+    file { '/etc/sudoers':
+        mode   => 440,
+        owner  => root,
+        group  => root,
+        source => 'puppet:///modules/sudo/sudoers'
+    }
 }
