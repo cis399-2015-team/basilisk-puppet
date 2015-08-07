fdfa2e2022fd65857dedd484be8ba6ac00310ba4
hkhamm <hkhamm@gmail.com>
2015-08-07 15:44:34 -0700

    science!

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,20 +19,5 @@ node ip-10-0-3-110 {
     include postfix
     include apache2
     include smbclient
-
-    class { 'sudo': }
-    sudo::conf { 'admin':
-        ensure  => present,
-        content => '%admin ALL=(ALL) ALL',
-    }
-
-    sudo::conf { 'samba':
-        ensure  => present,
-        content => '%samba ALL=(ALL) /bin/mount,/bin/umount,/sbin/mount.cifs,/sbin/umount.cifs',
-    }
-
-    sudo::conf { 'sudo':
-        ensure  => present,
-        content => '%sudo ALL=(ALL:ALL) ALL',
-    }
+    include sudo
 }
